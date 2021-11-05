4935cb24784dfddb315891cdaab199a4e1fc1160
Cristian Benitez <cbenitezbusiness9@gmail.com>
2021-11-04 20:55:44 -0500

    fixed vscode/js bug

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -16,7 +16,7 @@ function GameCard({review, handleDelete}) {
             <img alt={review.title} src={review.image}/>
             <h3>{review.title}</h3>
             <h5>{review.rating} ⭐️'s</h5>
-            <h3 style={{color: "red"}}>${review.price}</h3>
+            <h3 style={{color: "red"}}>{"$" + review.price}</h3>
             <em><h4>Platform: {review.platform}</h4></em>
             <h5>"{review.review}"</h5>
             <p> -{review.user}</p>
